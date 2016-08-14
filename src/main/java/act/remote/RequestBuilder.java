c009dd4b2878229a366137899698b54e3b46403a
Gelin Luo <greenlaw110@gmail.com>
2016-08-14 18:07:16 +1000

    WIP

--- src/main/java/act/remote/RequestBuilder.java
+++ src/main/java/act/remote/RequestBuilder.java
@@ -1,15 +1,18 @@
 package act.remote;
 
 import io.undertow.util.HeaderMap;
+import io.undertow.util.HttpString;
 import org.osgl.http.H;
 import org.osgl.util.C;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import static act.remote.Protocol.*;
+import static org.osgl.http.H.Method.*;
 
 /**
  * Build the request to be sent out
@@ -23,6 +26,11 @@ public class RequestBuilder extends H.Request<RequestBuilder> {
     protected Map<String, List<String>> params = C.newMap();
     protected Map<String, H.Cookie> cookies = C.newMap();
     protected byte[] body;
+
+    /**
+     * The encoding default value is {@link RequestPayloadEncoding#DEFAULT}
+     */
+    protected RequestPayloadEncoding encoding = RequestPayloadEncoding.DEFAULT;
 
     public RequestBuilder() {
         init();
@@ -44,8 +52,43 @@ public class RequestBuilder extends H.Request<RequestBuilder> {
     }
 
     @Override
-    public H.Request method(H.Method method) {
+    public RequestBuilder method(H.Method method) {
         this.method = method;
+        return this;
+    }
+
+    public RequestBuilder addHeader(String name, String val) {
+        headers.add(httpString(name), val);
+        return this;
+    }
+
+    public RequestBuilder addHeader(String name, long val) {
+        headers.add(httpString(name), val);
+        return this;
+    }
+
+    public RequestBuilder addHeader(String name, Collection<String> vals) {
+        headers.addAll(httpString(name), vals);
+        return this;
+    }
+
+    public RequestBuilder putHeader(String name, String val) {
+        headers.put(httpString(name), val);
+        return this;
+    }
+
+    public RequestBuilder putHeader(String name, long val) {
+        headers.put(httpString(name), val);
+        return this;
+    }
+
+    public RequestBuilder putHeader(String name, Collection<String> vals) {
+        headers.putAll(httpString(name), vals);
+        return this;
+    }
+
+    public RequestBuilder addCookie(H.Cookie cookie) {
+        _setCookie(cookie.name(), cookie);
         return this;
     }
 
@@ -133,13 +176,59 @@ public class RequestBuilder extends H.Request<RequestBuilder> {
     }
 
     /**
-     * Returns a `RequestBuilder` with specified URL and a POJO instance as payload
+     * Returns a `RequestBuilder` with specified URL and provisioned with {@link org.osgl.http.H.Method#GET get} method
      * @param url the remote service URL
-     * @param pojo the payload
      * @return a `RequestBuilder` instance
      */
-    public static RequestBuilder get(String url, Object pojo) {
-        return null;
+    public static RequestBuilder get(String url) {
+        return newBuilder(GET);
     }
 
+    /**
+     * Returns a `RequestBuilder` with specified URL and provisioned with {@link org.osgl.http.H.Method#POST post} method
+     * @param url the remote service URL
+     * @return a `RequestBuilder` instance
+     */
+    public static RequestBuilder post(String url) {
+        return newBuilder(POST);
+    }
+
+    /**
+     * Returns a `RequestBuilder` with specified URL and provisioned with {@link org.osgl.http.H.Method#PUT put} method
+     * @param url the remote service URL
+     * @return a `RequestBuilder` instance
+     */
+    public static RequestBuilder put(String url) {
+        return newBuilder(PUT);
+    }
+
+    /**
+     * Returns a `RequestBuilder` with specified URL and provisioned with {@link org.osgl.http.H.Method#DELETE delete} method
+     * @param url the remote service URL
+     * @return a `RequestBuilder` instance
+     */
+    public static RequestBuilder delete(String url) {
+        return newBuilder(DELETE);
+    }
+
+    /**
+     * Returns an new `RequestBuilder` instance
+     * @return an new `RequestBuilder` instance
+     */
+    public static RequestBuilder newBuilder() {
+        return new RequestBuilder();
+    }
+
+    /**
+     * Create an new `RequestBuilder` instance with http method specified
+     * @param method the http method to provision the request builder
+     * @return the new request builder
+     */
+    public static RequestBuilder newBuilder(H.Method method) {
+        return new RequestBuilder().method(method);
+    }
+
+    private static HttpString httpString(String s) {
+        return new HttpString(s);
+    }
 }
--- src/main/java/act/remote/RequestPayloadEncoding.java
+++ src/main/java/act/remote/RequestPayloadEncoding.java
@@ -1,5 +1,7 @@
 package act.remote;
 
+import act.data.RequestBodyParser;
+
 /**
  * Defines the way to serialize structured payload to
  * request query or body.
@@ -11,7 +13,41 @@ package act.remote;
  * * `foo[]=1&foo[]=2&foo[]=3`
  * * `foo[0]=1&foo[1]=2&foo[2]=3`
  * * `foo.0=1&foo.1=2&foo.2=3`
+ *
+ * For more information please refer to
+ * https://github.com/actframework/act-remote/blob/master/doc/payload-encoding.md
  */
 public enum RequestPayloadEncoding {
+
+    NAME_ONLY,
+
+    NAME_BRACKET,
+
+    NAME_BRACKET_INDEX,
+
+    NAME_DOT_INDEX,
+
+    JSON,
+
+    /**
+     * JQuery encoding is just an alias of
+     * {@link #NAME_BRACKET}
+     */
+    JQUERY(NAME_BRACKET),
+
+    /**
+     * Default encoding is {@link #JSON}
+     */
+    DEFAULT(JSON)
+
+    ;
+
+    private RequestPayloadEncoding delegateEncoding;
+
+    private RequestPayloadEncoding() {this(null);}
+
+    private RequestPayloadEncoding(RequestPayloadEncoding delegate) {
+        this.delegateEncoding = delegate;
+    }
 
 }
